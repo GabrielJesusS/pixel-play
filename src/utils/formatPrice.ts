028b0afac819ad83a112e60f3658058937052f9c
Gabriel Jesus <gabriel.ds.jesus@gmail.com>
2024-03-11 19:10:26 -0300

    refactor: fixes format price

--- src/utils/formatPrice.ts
+++ src/utils/formatPrice.ts
@@ -1,6 +1,6 @@
 export function formatPrice(price: number): string {
   if (price > 0) {
-    return `R$ ${price.toPrecision(2).replace(".", ",")}`;
+    return `R$ ${price.toFixed(2).replace(".", ",")}`;
   }
 
   return "Grátis";
